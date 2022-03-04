696ad5128f82938cc33f2274653d30ab8ab162fc
Etienne Ruffieux <eruffieux@google.com>
2022-03-04 13:27:02 +0000

    msm8998-common: Removed audio audio A2dp and Hearing Aid from common.mk
    Bug: 220306859
    Test: atest
    Tag: #feature
    Change-Id: Ia1d4fbe0e406e895d295936c524c5dbba6c269cd

--- common.mk
+++ common.mk
@@ -85,9 +85,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl:32 \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.2-impl:32 \
-    audio.a2dp.default \
     audio.bluetooth.default \
-    audio.hearing_aid.default \
     audio.primary.msm8998 \
     audio.r_submix.default \
     audio.usb.default \
