2fbfd9f15d3f7bc98b60c2c5132fd73642d73be5
Timi Rautamäki <timi.rautamaki@gmail.com>
2023-03-08 18:46:06 +0000

    msm8998-common: import qcom hardware
    Change-Id: If31b1bea9298cc419774b50aa4f2df95c855661b

---
+++ qcom-caf/display/sdm/libs/utils/Android.mk
@@ -0,0 +1,17 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+include $(LOCAL_PATH)/../../../common.mk
+
+LOCAL_MODULE                  := libsdmutils
+LOCAL_VENDOR_MODULE           := true
+LOCAL_MODULE_TAGS             := optional
+LOCAL_HEADER_LIBRARIES        := display_headers
+LOCAL_CFLAGS                  := -DLOG_TAG=\"SDM\" $(common_flags)
+LOCAL_SRC_FILES               := debug.cpp \
+                                 rect.cpp \
+                                 sys.cpp \
+                                 formats.cpp \
+                                 utils.cpp
+
+LOCAL_SHARED_LIBRARIES        := libdisplaydebug
+include $(BUILD_SHARED_LIBRARY)
