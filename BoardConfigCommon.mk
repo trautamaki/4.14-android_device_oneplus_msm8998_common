14c67b63cdcdc62bc790717e0b112f8507d16623
Roberto Sartori <roberto.sartori.android@gmail.com>
2023-06-20 05:59:52 +0000

    msm8998-common: update command line for 4.14
    First commit to add support for kernel 4.14.
    
    Take these from Sony (Yoshino):
    - coherent_pool: increase from the default (256kB),
    - lpm_levels.sleep_disabled: improve boot speed, and it's only used
      during boot (it's disabled after that).
    
    Change-Id: I9813f430446e3a089fcc47d8d3f4b2f9c6c47267
    Signed-off-by: Roberto Sartori <roberto.sartori.android@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,7 +36,7 @@ BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 swiotlb=2048
-BOARD_KERNEL_CMDLINE += loop.max_part=7
+BOARD_KERNEL_CMDLINE += loop.max_part=16 coherent_pool=8M lpm_levels.sleep_disabled=1 kpti=0
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
